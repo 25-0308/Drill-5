9008b0304fbc158890e7975c00a709b6a242ab92
25-0308 <00rhkrtmdwns@hanmail.net>
2025-09-25 17:53:27 +0900

    배경 및 캐릭터 추가

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -1,3 +1,20 @@
 from pico2d import *
 
 open_canvas(800,600)
+
+bg=load_image('TUK_GROUND.png')
+character=load_image('korea_player_left.png')
+c_idle=load_image('player_idle_left.png')
+
+x,y=400,300
+
+def bg_n_face():
+    global x, y
+    face_frame = 0
+    clear_canvas()
+    bg.clip_draw(0, 0, 1280, 1024, 400, 300, 800, 600)
+    character.clip_draw(0, 0, 100, 100, x, y)
+    update_canvas()
+
+while True:
+    bg_n_face()
